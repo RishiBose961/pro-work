6a80609739ad6c179acdbfad47bb9b2573a1e6f3
unknown <rishibose1901@gmail.com>
2025-07-26 22:39:45 +0530

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import sitemap from "vite-plugin-sitemap";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ TanStackRouterVite(),react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    sitemap({
+      hostname: "https://rishibose.fun", 
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
